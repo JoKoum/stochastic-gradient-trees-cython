0d959524f715cbd4dfb86b31aa290e75e80efb95
John Koumentis <jokoum92@gmail.com>
2022-11-15 23:02:11 +0200

    attempt to clear memory

--- cysgt/utils/StreamingGradientTree.cpp
+++ cysgt/utils/StreamingGradientTree.cpp
@@ -306,7 +306,7 @@ void StreamingGradientTree::Node::applySplit(Split split)
         {
             mChildren[i] = new Node(mPrediction + split.deltaPredictions[i], mDepth + 1, mHasSplit, parentClass);
         }
-        children.clear();
+        std::vector<Node*>().swap(children);
     }
     else if (parentClass->mFeatureInfo[split.feature].type == ordinal)
     {
@@ -314,7 +314,7 @@ void StreamingGradientTree::Node::applySplit(Split split)
         mChildren = children;
         mChildren[0] =  new Node(mPrediction + split.deltaPredictions[0], mDepth + 1, mHasSplit, parentClass);
         mChildren[1] =  new Node(mPrediction + split.deltaPredictions[1], mDepth + 1, mHasSplit, parentClass);
-        children.clear();
+        std::vector<Node*>().swap(children);
     }
     else 
     {
